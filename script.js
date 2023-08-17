814f771a5c579a349f11cfb2959232be4684388a
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-08-17 19:23:41 +0500

    added more js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 // Event Listener
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 // Functions
 function addTodo(e) {
@@ -15,7 +16,7 @@ function addTodo(e) {
     todoDiv.classList.add("todo");
     // create li
     const newTodo = document.createElement("li");
-    newTodo.innerText = "hey";
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
     // check mark button
@@ -26,8 +27,25 @@ function addTodo(e) {
     // check trash button
     const trashButton = document.createElement("button");
     trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
-    trashButton.classList.add("complete-btn");
+    trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
     // append to list
     todoList.appendChild(todoDiv);
+    // clear input value
+    todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    // delete todo
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    // check mark
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
